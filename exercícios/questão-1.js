dd9470957074074f17aadd5ec4a31032fcebca07
Seidr <seidr@byseidr.com>
2024-12-17 00:53:25 -0300

    Adicionar resolução das questões 1 e 9 nos exercícios

---
+++ exercícios/questão-1.js
@@ -0,0 +1,7 @@
+let num1 = 16;
+let num2 = 4;
+
+console.log(`A soma dos valores é: ${num1 + num2}`);
+console.log(`A multiplicação dos valores é: ${num1 * num2}`);
+console.log(`A subtração dos valores é: ${num1 - num2}`);
+console.log(`A divisão dos valores é: ${num1 / num2}`);
---
+++ exercícios/questão-9.js
@@ -0,0 +1,8 @@
+let amigos = ["Miguel", "Leonam", "Antony"];
+
+let pessoas = {
+    [amigos[0]]: 50,
+    [amigos[1]]: 27,
+    [amigos[2]]: 63,
+};
+console.log(pessoas);

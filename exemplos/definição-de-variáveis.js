e0dcf0633b60d2073b96a6f7f43829e63ad679b8
Seidr <seidr@byseidr.com>
2025-03-07 11:59:31 -0300

    Incluir exemplo de definição de variáveis com o var

---
+++ exemplos/definição-de-variáveis.js
@@ -0,0 +1,15 @@
+// DEFINIÇÃO DE VARIÁVEIS -----------------------------------------------------
+
+// Exemplo 1 - Criação de variável
+var idade = 25;
+console.log(idade); // Isso vai mostrar o número 25 na tela
+
+// Exemplo 2 - Atualização de variável
+idade = 26;
+console.log(idade); // Agora vai mostrar 26
+
+// Exemplo 3 - Armazenamento de operações
+var numero1 = 10;
+var numero2 = 5;
+var soma = numero1 + numero2;
+console.log(soma); // Mostra 15, o resultado da soma
